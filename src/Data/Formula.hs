0094dec7eadf3460436b33f8e1f59857eaa84362
Jos Kusiek <jos.kusiek@tu-dortmund.de>
2017-03-16 11:41:31 +0100

    eval is working.
    solutions does not terminate.

--- src/Data/Formula.hs
+++ src/Data/Formula.hs
@@ -1,5 +1,9 @@
 module Data.Formula
   ( Formula(..)
+  , Variable(..)
+  , fromEither
+  , toEither
+  , eval
   ) where
 
 infixl 3 :&,:|
@@ -15,6 +19,9 @@ fromEither (Right b) = Var2 b
 toEither :: Variable a b -> Either a b
 toEither (Var1 a) = Left a
 toEither (Var2 b) = Right b
+
+update :: Eq a => (a -> b) -> a -> b -> a -> b
+update f a b a' = if a == a' then b else f a'
 
 data Formula v1 v2
   = FFalse
@@ -32,24 +39,68 @@ data Formula v1 v2
   | NotIn v1 v2
   deriving (Eq, Show, Read)
 
+powerset :: Eq a => [a] -> [[a]]
+powerset (a:s) = if a `elem` s then ps else ps ++ map (a:) ps where
+  ps = powerset s
+powerset _ = [[]]
 
-data Var = P | P' deriving (Show, Read, Eq)
-data Status = Idle | Wait | Crit | Idle' | Wait' | Crit'
-  deriving (Show, Read, Eq)
+gen1 :: Int -> [Int]
+gen1 size = [0..(size-1)]
 
-blocking :: Var -> Formula Var Status
-blocking p = Ex (Var1 P') (P' `In` Crit :| (p :< P' :& P' `In` Wait))
+gen2 :: Int -> [[Int]]
+gen2 = powerset . gen1
 
-trans :: Var -> Formula Var Status
-trans p
-  =  (p `In` Idle :& p `In` Idle' :=> Ex (Var1 P') (P' `In` Wait))
-  :& (p `In` Idle :& p `In` Wait' :=> All (Var1 P') (p :/= P' :=> P' `In` Idle'))
-  :& (p `In` Idle :& p `In` Crit' :=> FFalse)
-  :& (p `In` Wait :& p `In` Idle' :=> FFalse)
-  :& (p `In` Wait :& p `In` Wait' :=> blocking p)
-  :& (p `In` Wait :& p `In` Crit' :=> Neg (blocking p))
-  :& (p `In` Crit :& p `In` Idle' :=> FTrue)
-  :& (p `In` Crit :& p `In` Wait' :=> FFalse)
-  :& (p `In` Crit :& p `In` Crit' :=> FFalse)
+genStore1 :: Eq v1 => Int -> [v1] -> [v1 -> Int]
+genStore1 size = foldUpd undefined where
+  foldUpd f (a:as) = [ upd
+    | v1 <- genStore1' a f
+    , upd <- foldUpd v1 as
+    ]
+  foldUpd _ []     = []
+  genStore1' var1 g = [ update g var1 res | res <- gen1 size]
+
+
+
+genStore2 :: Eq v2 => Int -> [v2] -> [v2 -> [Int]]
+genStore2 size = foldUpd undefined where
+  foldUpd f (a:as) = [ upd
+    | v2 <- genStore2' a f
+    , upd <- foldUpd v2 as
+    ]
+  foldUpd _ []     = []
+  genStore2' var2 g = [ update g var2 res | res <- gen2 size]
+
+eval
+  :: (Eq v1, Eq v2)
+  => Int -> (v1 -> Int) -> (v2 -> [Int]) -> Formula v1 v2 -> Bool
+eval _ _ _ FFalse                = False
+eval _ _ _ FTrue                 = True
+eval size v1 v2 (Neg f)          = not (eval size v1 v2 f)
+eval _ v1 _ (x := y)             = v1 x == v1 y
+eval _ v1 _ (x :/= y)            = v1 x /= v1 y
+eval _ v1 _ (x :< y)             = v1 x < v1 y
+eval size v1 v2 (f1 :& f2)       = eval size v1 v2 f1 && eval size v1 v2 f2
+eval size v1 v2 (f1 :| f2)       = eval size v1 v2 f1 || eval size v1 v2 f2
+eval size v1 v2 (f1 :=> f2)      
+  = not (eval size v1 v2 f1) || eval size v1 v2 f2
+eval size v1 v2 (Ex (Var1 v) f)  = any (\v1upd -> eval size v1upd v2 f)
+  [ update v1 v res | res <- gen1 size]
+eval size v1 v2 (Ex (Var2 v) f)  = any (\v2upd -> eval size v1 v2upd f)
+  [ update v2 v res | res <- gen2 size]
+eval size v1 v2 (All (Var1 v) f) = all (\v1upd -> eval size v1upd v2 f)
+  [ update v1 v res | res <- gen1 size]
+eval size v1 v2 (All (Var2 v) f) = all (\v2upd -> eval size v1 v2upd f)
+  [ update v2 v res | res <- gen2 size]
+eval _ v1 v2 (a `In` as)         = v1 a `elem` v2 as
+eval _ v1 v2 (a `NotIn` as)      = v1 a `notElem` v2 as
+
+solutions :: (Eq v1, Eq v2) => [v1] -> [v2] -> Formula v1 v2 -> [([Int],[[Int]])]
+solutions vars1 vars2 f = [ (map v1 vars1, map v2 vars2)
+  | size <- [1..]
+  , v1 <- genStore1 size vars1
+  , v2 <- genStore2 size vars2
+  , eval size v1 v2 f
+  ]
+
 
 
--- test/Data/FormulaTest.hs
+++ test/Data/FormulaTest.hs
@@ -11,8 +11,38 @@ import Test.Tasty.HUnit
 tests :: TestTree
 tests = $(testGroupGenerator)
 
---prop_twice :: Int -> Bool
---prop_twice i = someFunction i == (2 * i)
+data Var = P | P' deriving (Show, Read, Eq)
+data Status = Idle | Wait | Crit | Idle' | Wait' | Crit'
+  deriving (Show, Read, Eq)
 
---case_twenty :: Assertion
---case_twenty = someFunction 10 @?= 20
+blocking :: Var -> Formula Var Status
+blocking p = Ex (Var1 P') (P' `In` Crit :| (p :< P' :& P' `In` Wait))
+
+trans :: Var -> Formula Var Status
+trans p
+  =  (p `In` Idle :& p `In` Idle' :=> Ex (Var1 P') (P' `In` Wait))
+  :& (p `In` Idle :& p `In` Wait' :=> All (Var1 P') (p :/= P' :=> P' `In` Idle'))
+  :& (p `In` Idle :& p `In` Crit' :=> FFalse)
+  :& (p `In` Wait :& p `In` Idle' :=> FFalse)
+  :& (p `In` Wait :& p `In` Wait' :=> blocking p)
+  :& (p `In` Wait :& p `In` Crit' :=> Neg (blocking p))
+  :& (p `In` Crit :& p `In` Idle' :=> FTrue)
+  :& (p `In` Crit :& p `In` Wait' :=> FFalse)
+  :& (p `In` Crit :& p `In` Crit' :=> FFalse)
+
+safety, safety' :: Var -> Var -> Formula Var Status
+safety p p'  = p `In` Crit :& p' `In` Crit
+safety' p p' = p `In` Crit' :& p' `In` Crit'
+
+phiSafety :: Formula Var Status
+phiSafety = safety P P' :& trans P :& trans P' :=> safety' P P'
+
+case_safety :: Assertion
+case_safety = assert $ eval 2 store1 store2 phiSafety where
+  store1 P  = 0
+  store1 P' = 1
+
+  store2 Idle = [0,1]
+  store2 Idle' = [1]
+  store2 Wait' = [0]
+  store2 _     = []
